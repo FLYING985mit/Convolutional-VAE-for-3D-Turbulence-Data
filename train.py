dc3b1bde3e54cfa0b264e184d705fadfb49c3683
FLYING985mit <147385752+FLYING985mit@users.noreply.github.com>
2025-07-09 16:19:59 +0800

    更新 .gitignore 文件以忽略运行和日志文件，添加训练日志内容并注释掉训练脚本中的日志配置

--- train.py
+++ train.py
@@ -4,19 +4,19 @@ from loss import loss_function
 import pdb
 import logging
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    filename='train.log',
-                    filemode='w')
+# logging.basicConfig(level=logging.INFO,
+#                     format='%(asctime)s - %(levelname)s - %(message)s',
+#                     filename='train.log',
+#                     filemode='w')
 
-logging.addLevelName(25,"Mu")
-def Mu(self, message, *args, **kwargs):
-    if self.isEnabledFor(25):
-        self._log(25, message, args, **kwargs)
+# logging.addLevelName(25,"Mu")
+# def Mu(self, message, *args, **kwargs):
+#     if self.isEnabledFor(25):
+#         self._log(25, message, args, **kwargs)
 
-logging.Logger.mu = Mu
+# logging.Logger.mu = Mu
 
-mu=logging.getLogger(__name__)
+# mu=logging.getLogger(__name__)
 
 def train(epoch, model, train_loader, kl_weight, optimizer, device, scheduler, args):
     """
@@ -28,7 +28,7 @@ def train(epoch, model, train_loader, kl_weight, optimizer, device, scheduler, a
     train_BCE_loss = 0
     train_KLD_loss = 0
 
-    logging.info("entered batch training")
+    # logging.info("entered batch training")
     print("train device:", device)
     for batch_idx, data in tqdm(enumerate(train_loader), total=len(train_loader), desc='train'):
 
@@ -41,7 +41,7 @@ def train(epoch, model, train_loader, kl_weight, optimizer, device, scheduler, a
         # call CVAE model
         # feeding 3D volume to Conv3D: https://discuss.pytorch.org/t/feeding-3d-volumes-to-conv3d/32378/6
         recon_batch, mu, logvar, _ = model(data)
-        logging.info(f"Epoch: {epoch} Batch {batch_idx}: mu : {mu}\nlogvar : {logvar}")
+        # logging.info(f"Epoch: {epoch} Batch {batch_idx}: mu : {mu}\nlogvar : {logvar}")
 
         # compute batch losses
         total_loss, BCE_loss, KLD_loss = loss_function(recon_batch, data, mu, logvar, kl_weight)
