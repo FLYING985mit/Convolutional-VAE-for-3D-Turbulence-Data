9ab22e4e5e77440abb0602d1e9c8c33d14e2ab0f
FLYING985mit <147385752+FLYING985mit@users.noreply.github.com>
2025-07-09 18:15:53 +0800

    调整损失函数中的 MSE 和 KL 散度计算方式，修正权重应用逻辑

--- loss.py
+++ loss.py
@@ -37,15 +37,15 @@ def loss_function(recon_x, x, mu, logvar, kl_weight):
     # CE = torch.nn.CrossEntropyLoss()(recon_x, x)
     # MSE = torch.nn.MSELoss(reduction='mean')(recon_x, x)
 
-    # MSE = 0.1 * torch.nn.MSELoss(reduction='mean')(recon_x, x)
-    MSE =  torch.nn.MSELoss(reduction='sum')(recon_x, x)
+    MSE = 0.1 * torch.nn.MSELoss(reduction='sum')(recon_x, x)
+    # MSE =  torch.nn.MSELoss(reduction='sum')(recon_x, x)
 
     # BCE = F.binary_cross_entropy(recon_x, x, reduction="mean") # only takes data in range [0, 1]
     # BCEL = torch.nn.BCEWithLogitsLoss(reduction="mean")(recon_x, x)
 
     # KL divergence loss (with annealing)
-    # KLD = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp()) # sum or mean
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    KLD = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp()) # sum or mean
+    # KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     # print("KLD:", KLD)
     KLD = KLD * kl_weight
 
